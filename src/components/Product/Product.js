5323a39986edc1fad806c25c768a9a34d6de8dd7
Ewa Olczak <ewa.olczak@gmail.com>
2023-01-05 23:31:45 +0100

    Add OptionColor component

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,12 +1,13 @@
 import styles from './Product.module.scss';
-import clsx from 'clsx';
 import Button from '../Button/Button';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 import shortid from 'shortid';
 import ProductImage from '../ProductImage/ProductImage';
+import OptionColor from '../OptionColor/OptionColor';
 
 const Product = (props) => {
+  // eslint-disable-next-line no-unused-vars
   const [currentColor, setCurrentColor] = useState(props.data.colors[0]);
   const [currentSize, setCurrentSize] = useState(props.data.sizes[0].name);
   const [currentPrice, setCurrentPrice] = useState(
@@ -15,11 +16,7 @@ const Product = (props) => {
 
   console.log(currentColor, currentSize, currentPrice);
 
-  const prepareColorClassName = (color) => {
-    return styles[
-      'color' + color[0].toUpperCase() + color.substr(1).toLowerCase()
-    ];
-  };
+  
 
   const getPrice = () => {
     return props.data.basePrice + currentPrice;
@@ -74,22 +71,7 @@ const Product = (props) => {
               ))}
             </ul>
           </div>
-          <div className={styles.colors}>
-            <h3 className={styles.optionLabel}>Colors</h3>
-            <ul className={styles.choices}>
-              {props.data.colors.map((color) => (
-                <li key={shortid()}>
-                  <button
-                    type='button'
-                    className={clsx(
-                      prepareColorClassName(color),
-                      currentColor === color ? styles.active : undefined
-                    )}
-                    onClick={() => setCurrentColor(color)}></button>
-                </li>
-              ))}
-            </ul>
-          </div>
+          <OptionColor colors={props.data.colors}/>
           <Button
             type='button'
             className={styles.button}
